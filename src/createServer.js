025970db79ed1024fc585e0dfa66ac4669c58a33
an-marryKyslenko <olih.a@icloud.com>
2025-01-15 22:57:43 +0100

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,25 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const baseUrl = new URL(req.url, `hppt://${req.headers.host}`);
+    const parts = baseUrl.pathname.split('/').filter((u) => u);
+    const query = Object.fromEntries(baseUrl.searchParams.entries());
+    
+    const result = {
+      parts,
+      query,
+    };
+
+    console.log(result);
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
 }
 
 module.exports = {
